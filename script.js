2bf7cfee4d5be28957f7caa054cfb293c0559e3e
holmesr19 <holmesr19@mail.wlu.edu>
2018-03-05 16:08:51 -0500

    added parts 4 and 5

--- script.js
+++ script.js
@@ -41,3 +41,57 @@ var part3message2 = "The length of the hypotenuse of a triangle with base " + tr
 
 document.getElementById("third").innerHTML = part3message1 + "<br>" + part3message2;
 triangle.drawIt();
+
+
+//Part 4: User entered base and height
+
+var userbase;
+var userheight;
+var part4msg1;
+function assignUserbase(){
+  userbase = prompt("Please enter a base size " + 
+                      "that is equal to or greater than 10: ");
+  userbase = Number(userbase);
+}
+
+function assignUserheight(){
+  userheight = prompt("Please enter a height " + 
+                      "that is equal to or greater than 0: ");
+  userbase = Number(userheight);
+}
+
+assignUserbase();
+
+//alerts not working, how to loop until both are satisfactory
+if (userbase < 10){
+  alert(userbase + "< 10!!")
+  assignUserbase();
+} else if (userbase >= 10){
+  assignUserheight();
+  if (userheight < 0){
+    alert(userheight + "<= 0!!")
+    assignUserheight();
+    }
+  else if (userheight > 0){
+    triangle.base = userbase;
+    triangle.height = userheight;
+    part4msg1 = "The area of a triangle with height "  + triangle.height + 
+      " and base " + triangle.base + " is " + triangle.calcarea();
+    part4msg2 = "The hypotenuse is of a triangle with height "  + triangle.height +
+      " and base " + triangle.base + " is " + triangle.calchypotenuse();
+  }
+}
+
+document.getElementById("fourth").innerHTML = part4msg1 + "<br>" + part4msg2;
+triangle.canvasId = "canvas2";
+triangle.drawIt();
+
+//Part 5: lots of triangles
+
+document.getElementById("fifth").innerHTML = "Growing Triangles"
+for (i = 1; i <= 5; i++){
+  triangle.base = 30 * i;
+  triangle.height = 20 * i;
+  triangle.canvasId = "canvas3";
+  triangle.drawIt();
+}
